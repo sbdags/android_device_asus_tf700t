ef4366a6202a732861b734e40be814a8b87f902b
Joshua <lj50036@users.noreply.github.com>
2014-02-19 02:53:38 -0600

    Update crombix_tf700t.mk

--- crombix_tf700t.mk
+++ crombix_tf700t.mk
@@ -29,11 +29,11 @@ $(call inherit-product, device/asus/tf700t/device_tf700t.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := CROMBI-X_tf700t
-PRODUCT_BRAND := asus
+PRODUCT_NAME := crombix_tf700t
+PRODUCT_BRAND := _that+
 PRODUCT_DEVICE := tf700t
 PRODUCT_MODEL := ASUS Transformer Pad TF700T
-PRODUCT_MANUFACTURER := asus
+PRODUCT_MANUFACTURER := sdbags+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.2.1/JOP40D/US_epad-10.6.1.14.10-20130801:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.2.1 JOP40D US_epad-10.6.1.14.10-20130801 release-keys"
 
 # Release name and versioning
